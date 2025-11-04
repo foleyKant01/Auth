5120c6225003552df55cff50aa0b74280facdf28
foleykant <krayediego@gmail.com>
2025-11-04 12:41:47 +0000

    OK

--- back/app.py
+++ back/app.py
@@ -5,17 +5,8 @@ from flask_restful import Resource, Api
 from config.db import db
 from config.constant import *
 from model.tt import *
-from resources.admin import AdminApi
 from resources.users import UsersApi
-from resources.teller import TellerApi
-from resources.advertisement import AdvertisementApi 
-from resources.business import BusinessApi
-from resources.categories import CategoriesApi 
-from resources.historiques import HistoriquesApi  
-from resources.favoris import FavorisApi 
 # from resources.combi import CombiApi 
-from resources.reports import ReportsApi
-from resources.stats import StatsApi
 from flask_migrate import Migrate
 from flask_cors import CORS
 
@@ -50,17 +41,8 @@ def home():
     return render_template('index.html')
 
 
-api.add_resource(AdminApi, '/api/admin/<string:route>', endpoint='all_admin', methods=['GET', 'POST', 'DELETE', 'PATCH'])
 api.add_resource(UsersApi, '/api/user/<string:route>', endpoint='all_user', methods=['GET', 'POST', 'DELETE', 'PATCH'])
-api.add_resource(TellerApi, '/api/teller/<string:route>', endpoint='all_teller', methods=['GET', 'POST', 'DELETE', 'PATCH'])
-api.add_resource(AdvertisementApi, '/api/advertisement/<string:route>', endpoint='all_advertisement', methods=['GET', 'POST', 'DELETE', 'PATCH']) 
-api.add_resource(BusinessApi, '/api/business/<string:route>', endpoint='all_business', methods=['GET', 'POST', 'DELETE', 'PATCH'])
-api.add_resource(CategoriesApi, '/api/categorie/<string:route>', endpoint='all_categiries', methods=['GET', 'POST', 'DELETE', 'PATCH'])
-api.add_resource(HistoriquesApi, '/api/historiques/<string:route>', endpoint='all_historiques', methods=['GET', 'POST', 'DELETE', 'PATCH'])
-api.add_resource(FavorisApi, '/api/favoris/<string:route>', endpoint='all_favoris', methods=['GET', 'POST', 'DELETE', 'PATCH']) 
-api.add_resource(ReportsApi, '/api/reports/<string:route>', endpoint='all_reports', methods=['GET', 'POST', 'DELETE', 'PATCH']) 
-api.add_resource(StatsApi, '/api/stats/<string:route>', endpoint='all_stats', methods=['GET', 'POST', 'DELETE', 'PATCH'])
-# api.add_resource(CombiApi, '/api/combi/<string:route>', endpoint='all_combi', methods=['GET', 'POST', 'DELETE', 'PATCH'])
+
 @app.route("/api/test", methods=["GET"])
 def test():
     return {"message": "API OK"}
--- back/resources/combi.py
+++
@@ -1,8 +0,0 @@
-# from flask_restful import Resource
-# from helpers.combi import *
-
-
-# class CombiApi(Resource):
-#     def post(self, route):
-#         if route == "combined_search":
-#             return combined_search()
